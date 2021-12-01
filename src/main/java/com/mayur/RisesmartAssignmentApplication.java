40cf1b90300cbdc746ce55ca544417addea15ad8
Mayur-Coditas <mayur.gaikwad@coditas.com>
2021-12-01 15:50:34 +0530

    Initial Commit

---
+++ src/main/java/com/mayur/RisesmartAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.mayur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisesmartAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisesmartAssignmentApplication.class, args);
+	}
+
+}
